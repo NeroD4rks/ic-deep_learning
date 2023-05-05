986483901e5651501737d2f0e115ad16c29b157c
Henrique <henriquevcost4@hotmail.com>
2023-05-05 08:45:21 -0300

    upload dataset

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import os
+
 from tensorflow import keras
 from objects.datasets_Info import DatasetsInfo
 
 
-dt = DatasetsInfo()
+def run(root_input, root_output):
+    dt = DatasetsInfo()
+    shape = (32, 32, 3)
+    cmap_list = ['binary', 'plasma', 'seismic', 'terrain', 'Paired']  # Paired tem que ser maiúsculo
+    img_list = ["CWT", "MTF", "RP", "GAF", "GAF_DIFF", "MTF_GAF_RP"]
+    dirs = list(filter(lambda element: os.path.isdir(os.path.join(root_input, element)), os.listdir(root_input)))
 
-representation = "CWT-BINARY"
+    for img in img_list:
+        for colormap in cmap_list:
+            start_output_path = os.path.join(root_output, img.upper() + "-" + colormap.upper())
+            for dir in dirs:
+                print(dir, img, colormap)
+                input_path = os.path.join(root_input, dir)
+                output_path = os.path.join(start_output_path, dir)
+                transform_train_and_test_into_images(img, colormap, input_path, output_path)
 
-shape = (32,32,3)
+                dt.iteration_dataset(output_path, representation, dir, shape)
 
-dt.iteration_datasets(representation, shape)
+                os.remove(output_path)
--- objects/datasets_Info.py
+++ objects/datasets_Info.py
@@ -3,6 +3,7 @@ from modelos import *
 from auxfunc import *
 from keras.utils import np_utils
 
+
 class DatasetsInfo:
 
     def __init__(self):
@@ -16,7 +17,33 @@ class DatasetsInfo:
             "alexnet": get_model_alexnet, 
             "vgg": get_model_vgg
         }
-    
+
+    def iteration_dataset(self, input_root: str, representation, type_image, shape) -> None:
+        results = []
+
+        row_to_add = {"representation": representation, "type_image": type_image}
+        test_folder = f"{input_root}\TEST"
+        train_folder = f"{input_root}\TRAIN"
+        log_debug(f"\n\nAnalisando e processando ***{type_image}*** do {representation}")
+        n_class = get_n_class(train_folder)
+        x_train, y_train = get_test_train(train_folder, shape)
+        x_test, y_test = get_test_train(test_folder, shape)
+
+        y_train = np_utils.to_categorical(y_train)
+        y_test = np_utils.to_categorical(y_test)
+
+        log_debug("Iniciando preprocess, x_train")
+        x_train = preprocess(x_train, shape)
+
+        log_debug("Iniciando preprocess, x_test")
+        x_test = preprocess(x_test, shape)
+
+        results_model = self.execute_models(x_train, y_train, x_test, y_test, shape, n_class)
+        for model, result_model in results_model.items():
+            row_to_add[model] = result_model
+
+        results.append(row_to_add)
+
     def iteration_datasets(self, representation: str, shape) -> None:
         list_types = os.listdir(Path(f"datasets\{representation}"))
         results = []
